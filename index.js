77fbdfbb432256e6d1891ded00ef5753712b3026
Ayoade Ibrahim <ayoadeibrahim84@gmail.com>
2021-05-20 20:14:32 +0100

    solved

--- index.js
+++ index.js
@@ -1,64 +1,3 @@
-console.log("**********CheckYuGiOh***********");
-function checkYuGiOh(wholeNumber){
-    let integer = []; 
-    
-    function check() {
-        for(var i = 1; i <= wholeNumber; i++){
-        integer.push(i);
-        
-        if((i % 2 === 0) && (i % 3 === 0) && (i % 5 === 0)){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "yu-gi-oh");
-            }else if((i % 2 === 0)  && (i % 3 === 0) && (i % 5 !== 0)){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "yu-gi");
-            }else if((i % 2 === 0) && (i % 3 !== 0) &&(i % 5 === 0)){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "yu-oh")
-            }else if((i % 2 !== 0) && (i % 3 === 0) && (i % 5 === 0)){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "gi-oh")
-            }else if(i % 2 === 0){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "yu");
-            }else if(i % 3 === 0){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "gi")
-            }else if(i % 5 === 0){
-                let flow = integer.indexOf(i)
-                integer.splice(flow, flow, "oh");
-            }    
-
-        }
-    }
-
-if(typeof(wholeNumber) !== "number") {
-    if (Array.isArray(wholeNumber)) {
-        return `invalid parameter: ${JSON.stringify(wholeNumber)}`;
-    }else if (typeof(wholeNumber) === "string") {
-        if (typeof(parseInt(wholeNumber)) === "number" && !isNaN(parseInt(wholeNumber))) {
-                check();
-            }else {
-                return `invalid parameter: ${JSON.stringify(wholeNumber)}`;
-            }
-    }else  {
-        return `invalid parameter: ${JSON.stringify(wholeNumber)}`;
-    }
-}else {
-        check();
-    }
-
-// console.log(integer);
-return integer;
-
-}
-console.log(checkYuGiOh(10));
-console.log(checkYuGiOh("5"));
-console.log(checkYuGiOh("fizzbuzz is meh"));
-console.log(checkYuGiOh("100"));
-console.log(checkYuGiOh("first"));
-
-
 console.log("**********ConvertFahrToCelsius***********");
 
 
@@ -99,3 +38,63 @@ console.log(convertFahrToCelsius("first"));
 console.log(convertFahrToCelsius(true));
 
 
+console.log("**********CheckYuGiOh***********");
+    function checkYuGiOh(wholeNumber){
+        let integer = []; 
+        
+        function check() {
+            for(var i = 1; i <= wholeNumber; i++){
+            integer.push(i);
+            
+            if((i % 2 === 0) && (i % 3 === 0) && (i % 5 === 0)){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "yu-gi-oh");
+                }else if((i % 2 === 0)  && (i % 3 === 0) && (i % 5 !== 0)){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "yu-gi");
+                }else if((i % 2 === 0) && (i % 3 !== 0) &&(i % 5 === 0)){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "yu-oh")
+                }else if((i % 2 !== 0) && (i % 3 === 0) && (i % 5 === 0)){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "gi-oh")
+                }else if(i % 2 === 0){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "yu");
+                }else if(i % 3 === 0){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "gi")
+                }else if(i % 5 === 0){
+                    let flow = integer.indexOf(i)
+                    integer.splice(flow, flow, "oh");
+                }    
+
+            }
+        }
+
+    if(typeof(wholeNumber) !== "number") {
+        if (Array.isArray(wholeNumber)) {
+            return `invalid parameter: ${JSON.stringify(wholeNumber)}`;
+        }else if (typeof(wholeNumber) === "string") {
+            if (typeof(parseInt(wholeNumber)) === "number" && !isNaN(parseInt(wholeNumber))) {
+                    check();
+                }else {
+                    return `invalid parameter: ${JSON.stringify(wholeNumber)}`;
+                }
+        }else  {
+            return `invalid parameter: ${JSON.stringify(wholeNumber)}`;
+        }
+    }else {
+            check();
+        }
+    
+    // console.log(integer);
+    return integer;
+
+}
+console.log(checkYuGiOh(10));
+console.log(checkYuGiOh("5"));
+console.log(checkYuGiOh("first"));
+console.log(checkYuGiOh("fizzbuzz is meh"));
+console.log(checkYuGiOh([1, 2, "desi", "pi"]));
+console.log(checkYuGiOh({"rem": 2}));
